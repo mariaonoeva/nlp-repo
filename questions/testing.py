02f2fec40ce4fc1899a89bcd4c5e83a1f473af30
Maria Onoeva <onoevam@ff.cuni.cz>
2025-04-14 17:26:59 +0200

    Spacy notebook

--- questions/testing.py
+++
@@ -1,8 +0,0 @@
-import spacy
-
-nlp = spacy.load("ru_core_news_sm")
-with open('/Users/maria.onoeva/Desktop/new_folder/GitHub/nlp-repo/questions/txts/HP_1ru.txt', 'r') as file:
-    text_ru = file.read()
-
-doc = nlp(text_ru)
-print(text_ru)
